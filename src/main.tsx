8c6cfeefd164dabc406fc2abbc88fc85248092ca
Yazan Alhalabi <yazanalhalabi1@gmail.com>
2024-05-10 20:35:22 +0300

    add shadcn and querystack

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-import { store } from './redux/store'
 import App from './App'
 import './index.css'
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <Provider store={store}>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </Provider>
+  </QueryClientProvider>
 )
