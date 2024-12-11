b1612d7a76f7506a0b915303855e27191a56e2b8
thoanguyen2907 <thoanguyen290795@gmail.com>
2024-12-11 12:44:55 +0700

    redux setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
-import ReactDOM from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import App from './App'
-import './index.css'
-
-const queryClient = new QueryClient()
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <QueryClientProvider client={queryClient}>
+  <React.StrictMode>
     <App />
-  </QueryClientProvider>
-)
+  </React.StrictMode>
+);
